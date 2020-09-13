260bc5c424bbcd21d12a29b7dcb9fe46e3f94c55
suraj1501 <sksurajy15@gmail.com>
2020-09-13 12:04:51 +0530

    Code sent

---
+++ Demoproject/src/test/java/tests/Logintest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Logintest {
+  @Test
+  public void Testfirefox() {
+	  
+	  System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Java\\selenium\\drivers\\geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+
+	  
+	  driver.get("https://www.google.com/");	  
+	  driver.quit();
+  }
+}
